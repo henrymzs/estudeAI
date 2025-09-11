2f9e35deaa120fb31c2d8060a3fb2116052f94e7
Henry <henrykaua21@gmail.com>
2025-09-11 15:49:53 -0300

    feat: adicioona navegacao de tela no app.js para telas de register e login

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './screens/Home';
+import Register from './screens/Register';
+import Login from './screens/Login';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +13,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
+         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
