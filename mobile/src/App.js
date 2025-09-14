422d03247bedafdd3d3505dbf5e184679e59c741
Henry <henrykaua21@gmail.com>
2025-09-13 22:47:49 -0300

    refactor: refatora app.js para utilizar estrutura modularizada

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,21 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import Home from './screens/Home';
-import Register from './screens/Register';
-import Login from './screens/Login';
-
-const Stack = createStackNavigator();
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={Register} />
-         <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
