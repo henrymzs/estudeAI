1d7d510cf3e26b0b65ad3984a564da79059654e4
Henry <henrykaua21@gmail.com>
2025-11-12 23:07:06 -0300

    fix: corrige importacoes de arquivos nas telas do app

--- mobile/src/screens/auth/Register.js
+++ mobile/src/screens/auth/Register.js
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, SafeAreaView, StatusBar, ScrollView, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import { Ionicons } from "@expo/vector-icons";
-import { Button } from '../../components/button';
-import { Input } from '../../components/input';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
